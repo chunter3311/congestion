5b05f67afa9ead66400e597ea89b7ac8e2f8c31f
Cole Hunter <cole.joshua.hunter@outlook.com>
2020-12-03 14:53:59 -0600

    make it better

---
+++ backend/app/static/precache-manifest.217dca824e9cb5e223d6deb087efdfab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce08cc11bfb0a0cb240906b589e2c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "093326ccd38b23dbbf8f",
+    "url": "/static/css/main.f24687be.chunk.css"
+  },
+  {
+    "revision": "b0aef8c10dfcda5201f0",
+    "url": "/static/js/2.0a348690.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0a348690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093326ccd38b23dbbf8f",
+    "url": "/static/js/main.52c561d3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
